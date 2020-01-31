927f441b5716878e6c41df805cb22749429667e6
valdezDev <pvaldezdev@gmail.com>
2020-01-31 13:41:36 -0500

    wrote up schema for Post, User model, got mock queries working on graphql

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
+const User = require('./models/User');
+const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    test: String!
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    test: () => 'testtesttest'
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 }
 
@@ -18,7 +36,12 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen({ port: 5000 })
+mongoose
+.connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected!');
+    return server.listen({ port: 5000 });
+  }) 
   .then(res => {
     console.log(`Server running at port: ${res.url}`);
   })
