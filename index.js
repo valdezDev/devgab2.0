850cc769cf94ea7509985755ed39116230e93b7f
valdezDev <pvaldezdev@gmail.com>
2020-02-23 12:04:11 -0500

    port stored in var, preparing for deployment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
 const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
@@ -16,11 +18,14 @@ const server = new ApolloServer({
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     // console.log(req)
-    return server.listen(5000, "100.115.92.203");
+    return server.listen({ port: PORT });
   })
-  .then((res) => {
+  .then(res => {
     //console.log(res)
     console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err);
   });
